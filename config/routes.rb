d8761de3d0e16fca380bbeeb64d1d65944d0d23e
lpg-tech <lpg@ganimedes.ch>
2022-02-23 10:52:43 +0100

    Sample Application with Bootstrap and Devise

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/faq'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "pages#home"
+end
