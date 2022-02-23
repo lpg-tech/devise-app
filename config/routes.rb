400ea171ba11e406ff5d6ac14656152cfbdd2bff
lpg-tech <lpg@ganimedes.ch>
2022-02-23 11:24:11 +0100

    refactor routes and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/faq'
+  root 'pages#home'
+  get '/about', to: 'pages#about'
+  get '/faq', to: 'pages#faq'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#home"
 end
